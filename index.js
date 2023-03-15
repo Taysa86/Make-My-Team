448ed57e64050ed2bb121cf589d6a4e80514b00d
Taysa86 <Taysarolu86@gmail.com>
2023-03-15 13:19:23 -0500

    added library for classes

---
+++ index.js
@@ -0,0 +1,11 @@
+//--- requires and consts
+
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Employee = require("./lib/employee");
+const Intern = require("./lib/intern");
+const generate = require("./src/sample");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const staff = [];
